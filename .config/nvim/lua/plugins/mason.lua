f3d7248741475699ce3766bd6b434b4f27811d33
Kleidi Bujari <mail@kleidi.ca>
2024-04-07 19:08:52 -0400

    feat: add config for yamlls and add cmdline back

--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -27,6 +27,7 @@ return {
         "svelte",
         "texlab",
         "tsserver",
+        "yamlls"
       },
       handlers = {
         function(server)
@@ -44,6 +45,26 @@ return {
                 },
               },
             },
+          })
+        end,
+
+        ["yamlls"] = function()
+          lc.yamlls.setup({
+            schemas = {
+              kubernetes = "*.yaml",
+              ["http://json.schemastore.org/github-workflow"] = ".github/workflows/*",
+              ["http://json.schemastore.org/github-action"] = ".github/action.{yml,yaml}",
+              ["https://raw.githubusercontent.com/ansible/ansible-lint/main/src/ansiblelint/schemas/ansible.json#/$defs/tasks"] =
+              "roles/tasks/*.{yml,yaml}",
+              ["https://raw.githubusercontent.com/ansible/ansible-lint/main/src/ansiblelint/schemas/ansible.json#/$defs/playbook"] =
+              "*play*.{yml,yaml}",
+              ["http://json.schemastore.org/kustomization"] = "kustomization.{yml,yaml}",
+              ["https://json.schemastore.org/dependabot-v2"] = ".github/dependabot.{yml,yaml}",
+              ["https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/schemas/v3.1/schema.json"] =
+              "*api*.{yml,yaml}",
+              ["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] =
+              "*docker-compose*.{yml,yaml}",
+            }
           })
         end,
 
