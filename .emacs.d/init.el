b96f129d922199c9658b08cecfe93b720d343888
Kleidi Bujari <mail@4kb.net>
2025-07-10 22:32:49 -0400

    init website publish

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -5,8 +5,6 @@
   (package-initialize)
   (load (concat user-emacs-directory "early-init.el")))
 
-(load-theme 'modus-vivendi t)
-
 (require 'package)
 
 (add-to-list 'package-archives
@@ -14,6 +12,8 @@
              '("NONGNU" . "https://elpa.nongnu.org/nongnu/"))
 
 (package-initialize)
+
+(load-theme 'modus-vivendi t)
 
 (when (< emacs-major-version 29)
   (unless (package-installed-p 'use-package)
@@ -25,7 +25,8 @@
       inhibit-splash-screen t
       blink-cursor-mode 0
       use-file-dialog nil
-      display-line-numbers-type 'relative)
+      display-line-numbers-type 'relative
+      compilation-scroll-output t)
 
 (setq-default indent-tabs-mode nil)
 (setq-default tab-width 2)
@@ -67,6 +68,8 @@
   (prog-mode . turn-on-evil-mode)
   (text-mode . turn-on-evil-mode))
 
+;;; Minibuffer and completions
+
 (use-package vertico
   :ensure t
   :hook (after-init . vertico-mode))
@@ -97,18 +100,79 @@
   :ensure nil
   :commands (dired)
   :hook
-  ((dired-mode . dired-hide-details-mode)
-   (dired-mode . hl-line-mode))
+  (dired-mode . hl-line-mode)
   :config
   (setq dired-recursive-copies 'always)
   (setq dired-recursive-deletes 'always)
   (setq delete-by-moving-to-trash t)
   (setq dired-dwim-target t))
 
-(use-package nix-mode :ensure t
+(use-package htmlize :ensure t)
+(require 'ox-publish)
+(setq blog-org-files-dir "~/org/")
+(defvar html-head
+  "<link rel=\"stylesheet\" href=\"/main.css\" type=\"text/css\">")
+
+(defvar html-preamble
+  "<header style=\" margin: 3em 0 3em 0; display: flex; gap: 8px; flex-direction: column; font-family: monospace;\">
+        <span>
+            Kleidi Bujari <<a href=\"mailto:mail@4kb.net\">mail@4kb.net</a>>
+        </span>
+        <nav style=\"display: flex; gap: 8px\">
+            <span><a href=\"/\">kleidi.ca</a> |</span>
+            <a href=\"/about.html\">about</a>
+            <a target=\"_blank\" href=\"https://github.com/kbujari\">git</a>
+            <a href=\"/feed.xml\">rss</a>
+        </nav>
+    </header>")
+
+(defun kleidi/org-sitemap-format-entry (entry style project)
+  "Format ENTRY in org-publish PROJECT."
+  (let ((filetitle (org-publish-find-title entry project)))
+    (if (= (length filetitle) 0)
+        (format "[[file:%sindex.html][*%s*]]" entry entry)
+      (format "%s [[file:%s][%s]]"
+              (format-time-string "%Y-%m-%d"
+                                  (org-publish-find-date entry project))
+              entry
+              filetitle))))
+
+(setq org-publish-project-alist
+      `(("pages"
+         :base-directory ,blog-org-files-dir
+         :base-extension "org"
+         :recursive t
+         :publishing-directory "~/public_html/"
+         :publishing-function org-html-publish-to-html
+         :auto-sitemap t
+         :sitemap-title nil
+         :sitemap-filename "index.org"
+         :sitemap-sort-files chronologically
+         :sitemap-format-entry kleidi/org-sitemap-format-entry
+         :html-head-include-default-style nil
+         :html-postamble nil
+         :html-head ,html-head
+         :html-preamble ,html-preamble)
+
+        ("css"
+         :base-directory ,blog-org-files-dir
+         :base-extension "css"
+         :publishing-directory "~/public_html/"
+         :publishing-function org-publish-attachment)
+
+        ("kleidi.ca"
+         :components ("pages" "css"))))
+
+;;; Languages
+
+(use-package nix-ts-mode
+  :ensure t
   :mode "\\.nix\\'")
 
-(use-package rust-mode :ensure t)
+(use-package rust-mode
+  :ensure t
+  :init
+  (setq rust-mode-treesitter-derive t))
 
 (use-package tuareg
   :ensure t)
